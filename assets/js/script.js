1471cebfa25bc2b6e6b9d97c91e9febdb32e9cac
unknown <geoffdotson@gmail.com>
2022-08-09 23:44:58 -0500

    Fixed hiding nav on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("top").style.visibility = "visible";
+//   } else {
+//     document.getElementById("top").style.visibility = "hidden";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
+
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+(window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("top").style.visibility = "visible";
+    document.getElementById("top").style.top = "0";
   } else {
-    document.getElementById("top").style.visibility = "hidden";
+    document.getElementById("top").style.top = "-80px";
   }
   prevScrollpos = currentScrollPos;
-};
+})();
